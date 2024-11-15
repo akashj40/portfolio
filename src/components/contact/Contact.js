e299b115a09fee036bed48e7964b9764f78e20ac
Akash J <132737364+akashj40@users.noreply.github.com>
2024-11-15 18:14:45 +0530

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import { AiOutlineDownload } from "react-icons/ai";
 import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "../../style.css";
@@ -16,13 +14,13 @@ function Contact() {
       <Particle />
       <Row className="justify-content-center"> 
          
-          <a // Use an <a> tag for the link
+          <a 
             href="mailto:akashjoshapath@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            className="contact-button" // Apply the CSS class
+            className="contact-button" 
           >
-            <span>Mail Me  </span> {/* Text inside the button */}
+            <span>Mail Me  </span> 
             <svg viewBox="0 0 13 10" height="10px" width="15px">
               <path d="M1,5 L11,5"></path>
               <polyline points="8 1 12 5 8 9"></polyline>
